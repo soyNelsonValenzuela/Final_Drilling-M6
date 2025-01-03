e06219952a4f715aa04780eb0d1e6fe9e16d524e
soyNelsonValenzuela <nelsonvalenzuelagomez@gmail.com>
2025-01-03 16:43:33 -0300

    modificacion parametro test actualizacion

--- test/index.test.js
+++ test/index.test.js
@@ -2,61 +2,35 @@ import chai from 'chai'; // Opción corregida
 import chaiHttp from 'chai-http';
 import { describe, it } from 'mocha';
 import { PORT } from '../index.js';
-
-chai.use(chaiHttp); // Esto habilita chai-http
+chai.use(chaiHttp);
 const { expect, request } = chai;
 const baseUrl = `http://localhost:${PORT}`;
 describe('API de Animes', () => {
     let idAnimeCreado;
     it('Debería mostrar un mensaje de bienvenida en la raiz (GET /)', (done) => {
         request(baseUrl)
-            .get('/')
-            .end((err, res) => {
-                expect(res).to.have.status(200);
-                done()
-            });
+        .get('/')
+        .end((err, res) => {
+            expect(res).to.have.status(200);
+            done()
+        });
     });
     it('Debería listar todos los animes (GET /animes)', (done) => {
         request(baseUrl)
-            .get('/animes')
-            .end((err, res) => {
-                expect(res).to.have.status(200);
-                expect(res.body).to.be.an('object');
-                done();
-            });
+        .get('/animes')
+        .end((err, res) => {
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an('object');
+            done();
+        });
     });
-
-    // Test para buscar un anime por ID
-    it('Debería obtener un anime por ID (GET /animes?id=1)', (done) => {
-        chai.request(baseUrl)
-            .get('/animes?id=1')
-            .end((err, res) => {
-                expect(res).to.have.status(200);
-                expect(res.body).to.be.an('object');
-                done();
-            });
-    });
-
-    // Test para buscar un anime por nombre
-    it('Debería obtener un anime por nombre (GET /animes?nombre=Dragon%20Ball)', (done) => {
-        chai.request(baseUrl)
-            .get('/animes?nombre=Dragon%20Ball')
-            .end((err, res) => {
-                expect(res).to.have.status(200);
-                expect(res.body).to.be.an('object');
-                done();
-            });
-    });
-
-    // Test para crear un nuevo anime
     it('Debería crear un nuevo anime (POST /agregar)', (done) => {
         const nuevoAnime = {
             "nombre": "One Piece",
             "genero": "Shonen",
             "anio": 1999,
             "autor": "Eiichiro Oda"
-          };
-
+        };
         chai.request(baseUrl)
             .post('/agregar')
             .send(nuevoAnime)
@@ -68,29 +42,42 @@ describe('API de Animes', () => {
                 done();
             });
     });
-
-    // Test para actualizar un anime existente
+    it('Debería obtener un anime por ID (GET /animes?id=:id)', (done) => {
+        chai.request(baseUrl)
+            .get(`/animes?id=${idAnimeCreado}`)
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.an('object');
+                done();
+            });
+    });
+    it('Debería obtener un anime por nombre (GET /animes?nombre=Dragon%20Ball)', (done) => {
+        chai.request(baseUrl)
+            .get('/animes?nombre=Dragon%20Ball')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.an('object');
+                done();
+            });
+    });
     it('Debería actualizar un anime existente (PUT /actualizar/:id)', (done) => {
         const actualizacion = {
             "nombre": "One Piece (Actualizado)",
             "genero": "Shonen",
             "anio": 1999,
             "autor": "Eiichiro Oda"
-          };
-
+        };
         chai.request(baseUrl)
-            .put('/actualizar/1')
+            .put(`/actualizar/${idAnimeCreado}`)
             .send(actualizacion)
             .end((err, res) => {
                 expect(res).to.have.status(200);
                 expect(res.body).to.be.an('object');
-                expect(res.body).to.have.property("mensaje","Anime actualizado exitosamente");
+                expect(res.body).to.have.property("mensaje", "Anime actualizado exitosamente");
                 done();
             });
     });
-
-     // Test para eliminar un anime existente
-     it('Debería eliminar un anime existente (DELETE /eliminar/:id)', (done) => {
+    it('Debería eliminar un anime existente (DELETE /eliminar/:id)', (done) => {
         chai.request(baseUrl)
             .delete(`/eliminar/${idAnimeCreado}`)
             .end((err, res) => {
@@ -100,5 +87,4 @@ describe('API de Animes', () => {
                 done();
             });
     });
-  
 });
